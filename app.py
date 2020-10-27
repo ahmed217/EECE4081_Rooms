fd3bf2187c56d3dcae3f8a2082700f4ccbffb1c9
hillac99 <70351433+hillac99@users.noreply.github.com>
2020-10-27 18:35:34 -0500

    JSON Dump

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import render_template       # render html5 templates
 from flask import request               #
 from flask import redirect              #
 from flask_sqlalchemy import SQLAlchemy # create sqlite databases using python3
+from flask.json import jsonify
 
 # create the flask application object
 app = Flask(__name__)
@@ -17,8 +18,16 @@ db = SQLAlchemy(app)
 class Rooms(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(16), nullable = False)
+    number = db.Column(db.Integer, nullable = False)
     capacity = db.Column(db.Integer, nullable = False)
     
+    def serialize(self):
+        return {
+            'id'        :   self.id,
+            'name'      :   self.name,
+            'number'    :   self.number,
+            'capacity'  :   self.capacity
+        }
 #################################################################
 # Create basic CRUD API                                         #
 #                                                               #
@@ -28,25 +37,32 @@ class Rooms(db.Model):
 # Delete - will delete a single entry of a broken laptop        #
 #################################################################
 
-@app.route('/')
+@app.route('/init_db')
 def init_db():
     db.drop_all()
     db.create_all()
-    return redirect('/read')
+    return 'DB initialized'
+
+@app.route('/json_dump')
+def json_dump():
+    all_rooms = Rooms.query.all()
+    json_data = jsonify( json_list = [i.serialize() for i in all_rooms] )
+    return json_data 
 
 @app.route('/create', methods=['GET','POST'])
 def create():
     if request.form:
         name = request.form.get("name")
+        number = request.form.get("number")
         capacity = request.form.get("capacity")
-        new_room = Rooms(name = name, capacity = capacity)
+        new_room = Rooms(name = name,number = number ,capacity = capacity)
         db.session.add(new_room)
         db.session.commit()
         
     all_rooms = Rooms.query.all()
     return render_template("create.html", all_rooms = all_rooms, title = "Create a Room")
 
-@app.route('/read')
+@app.route('/')
 def read():
     all_rooms = Rooms.query.all()
     return render_template("read.html", all_rooms = all_rooms, title = "Rooms Listing")
@@ -56,6 +72,7 @@ def update(room_id):
     update_room = Rooms.query.get(room_id)
     if request.form:
         update_room.name = request.form.get("name")
+        update_room.number = request.form.get("number")
         update_room.capacity = request.form.get("capacity")
         db.session.commit()
     all_rooms = Rooms.query.all()
@@ -66,7 +83,7 @@ def delete(room_id):
     delete_room = Rooms.query.get(room_id)
     db.session.delete(delete_room)
     db.session.commit()
-    return redirect("/read")
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
